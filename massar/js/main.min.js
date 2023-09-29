115cd4e39318bdbb0a9c6d0fc8fce8c03e4fc3d9
Mikhail Brazhnik <mikbrazh@gmail.com>
2023-09-29 14:49:53 +0300

    preloader

--- massar/js/main.min.js
+++ massar/js/main.min.js
@@ -116,5 +116,19 @@ if ( document.querySelector('.modal') ) {
 
 }
 /* ======= modal END ======= */
+
+/* ======= preloader START ======= */
+let hidePreloader = function() {
+	setTimeout(function() {
+	  let preloader = document.getElementById('preloader');
+	
+	  if (!preloader.classList.contains('preloader--hidden')) {
+		  preloader.classList.add('preloader--hidden');
+	  }
+	}, 300)
+  };
+  
+  document.addEventListener('DOMContentLoaded', hidePreloader());
+/* ======= preloader END ======= */
 /* ======= Main JS END ======= */
 })();
